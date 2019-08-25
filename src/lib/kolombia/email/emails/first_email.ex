1ac142800b8be3d6cd0184e296794395c691cf40
Enrique <llenriquelopez@gmail.com>
2019-08-24 19:24:40 -0500

    Ready to recibe params

--- src/lib/kolombia/email/emails/first_email.ex
+++ src/lib/kolombia/email/emails/first_email.ex
@@ -3,15 +3,15 @@ defmodule Kolombia.Emails.FirstEmail do
   import Bamboo.Email
   import Bamboo.SendGridHelper
 
-  def first_email do
+  def deposit_email(destiny, sender, params) do
     new_email()
     |>to("llenriquelopez@gmail.com")
     |> from("gshck@salinas.com")
-    |> add_dynamic_field(:amount, 800)
-    |> add_dynamic_field(:account, 123456743564)
+    |> add_dynamic_field(:amount, params.amount)
+    |> add_dynamic_field(:account, params.account)
     |> add_dynamic_field(:date, Date.utc_today)
-    |> add_dynamic_field(:reference, 98765433)
-    |> add_dynamic_field(:name, "Gabriel Garcia Marquez")
+    |> add_dynamic_field(:reference, params.reference)
+    |> add_dynamic_field(:name, params.name)
     |> with_template(Application.get_env(:kolombia, :sendgrid_transaction_id))
   end
 end
