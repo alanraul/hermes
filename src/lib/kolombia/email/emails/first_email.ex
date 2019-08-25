7737f1c6da749519d0496d25e7149a94ec87ad22
Enrique <llenriquelopez@gmail.com>
2019-08-24 19:26:59 -0500

    Removes plain variables

--- src/lib/kolombia/email/emails/first_email.ex
+++ src/lib/kolombia/email/emails/first_email.ex
@@ -3,10 +3,10 @@ defmodule Kolombia.Emails.FirstEmail do
   import Bamboo.Email
   import Bamboo.SendGridHelper
 
-  def deposit_email(destiny, sender, params) do
+  def deposit_email(destiny, params) do
     new_email()
-    |>to("llenriquelopez@gmail.com")
-    |> from("gshck@salinas.com")
+    |>to(destiny)
+    |> from("banco@azteca.com")
     |> add_dynamic_field(:amount, params.amount)
     |> add_dynamic_field(:account, params.account)
     |> add_dynamic_field(:date, Date.utc_today)
