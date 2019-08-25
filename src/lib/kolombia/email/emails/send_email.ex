bf2310282bfb2380bc719e7029c36599929d8089
Enrique <llenriquelopez@gmail.com>
2019-08-25 06:41:25 -0500

    Fix spec

--- src/lib/kolombia/email/emails/send_email.ex
+++ src/lib/kolombia/email/emails/send_email.ex
@@ -7,7 +7,7 @@ defmodule Kolombia.EmailSender do
   @doc """
   Recibe los parametros del deposito y envia el email
   """
-  @spec send_first_email(String.t(), map) :: %Bamboo.Email{}
+  @spec send_first_email(String.t(), map) :: Email.t()
   def send_first_email(destiny, params) do
     FirstEmail.deposit_email(destiny, params)
     |> Mailer.deliver_later()
