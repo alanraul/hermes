3caefd29d604039dc8abb2100d1df85dcb866c50
Enrique <llenriquelopez@gmail.com>
2019-08-25 05:22:17 -0500

    fix sms sending

--- src/lib/kolombia_web/controllers/notification_controller.ex
+++ src/lib/kolombia_web/controllers/notification_controller.ex
@@ -36,13 +36,15 @@ defmodule KolombiaWeb.NotificationController do
         {:error, Poison.decode!(error)}
     end
   end
+
   def notify("sms", transaction) do
     case SendSMS.create_message(transaction["to_message"], transaction["from_message"], transaction["amount"]) do
-      {:ok, sms} -> sms
+      {:ok, sms} -> {:ok, :sms}
       {:error, message} ->
         {:error, message}
     end
   end
+
   def notify(method, transaction) do
     {:error, %{
       method: method,
